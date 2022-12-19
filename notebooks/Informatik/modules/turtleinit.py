c69ed164dd4959beac5c5b8ebf90459857b70d1d
lmh <dev@hilberg.eu>
2022-12-19 09:24:17 +0100

    add custom turtle class

--- notebooks/Informatik/modules/turtleinit.py
+++ notebooks/Informatik/modules/turtleinit.py
@@ -6,8 +6,44 @@ turtle modules
 """
 import ipyturtle3 as ipyturtle
 from ipyturtle3 import hold_canvas
+import numpy as np
 import time
 import random
+
+class customturtle(ipyturtle.Turtle):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+
+    def gox(self,steps):
+        self.goto(self.position()[0]+steps,self.position()[1])
+
+    def goy(self,steps):
+        self.goto(self.position()[0],self.position()[1]+steps)
+    
+    def godown(self,steps):
+        self.goy(-steps)
+
+    def goup(self,steps):
+        self.goy(steps)
+
+    def goleft(self,steps):
+        self.gox(-steps)
+        
+    def goright(self,steps):
+        self.gox(steps)
+    
+    def goxy(self,x,y):
+        x,y = list(np.array(self.position()) + np.array([x,y]))
+        self.goto(x,y)
+    
+    def diagonal(self,angle,steps):
+        """
+        Walking a diagonal without rotating the turtle.
+        """
+        angle = angle/180*np.pi
+        x,y = list(np.array(self.position()) + np.array([np.cos(angle)*steps, np.sin(angle)*steps]))
+        self.goto(x,y)        
+
 
 def newturtle(width=400,height=250):
     # Hier legen wir fest wie der Hintergrund aussieht
@@ -18,7 +54,7 @@ def newturtle(width=400,height=250):
     myTS.bgcolor("lightgreen")
 
     # Schildi die Schildkröte wird erschaffen
-    turtle=ipyturtle.Turtle(myTS)
+    turtle=customturtle(myTS)
 
     myTS.delay(200)
     return turtle
