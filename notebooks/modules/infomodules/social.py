a1e9c5094352ace149f316067a44711e9ec3307b
lmh <dev@hilberg.eu>
2023-01-02 13:21:07 +0100

    update soziogramm

--- notebooks/modules/infomodules/social.py
+++ notebooks/modules/infomodules/social.py
@@ -3,16 +3,24 @@
 """
 @author: lmh
 """
-
 import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
+import os
 from itertools import combinations,chain
+from jinja2 import Environment, BaseLoader
+import shutil
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 
 class soziogramm(object):
     def __init__(self,**kwargs):
         self.config = {}
         self.config.update(kwargs)
+        
+        self.tmpdir = os.getcwd()
         
         self.names = None
         
@@ -20,12 +28,26 @@ class soziogramm(object):
         self.fig = None
         self.cons = None
         self.clique = None
+        
+        self.template = ""
+        self.set_template()
+        
+        self.rendervars = {'cons':'', 'cliques':''}
 
     def read_names(self):
         self.names = pd.read_csv('namen.csv', header=None)
     
     def save(self, format='pdf', name='soziogramm'):
-        self.fig.savefig(name+"."+format)
+        self.fig.savefig(os.path.join(self.tmpdir,name+"."+format))
+    
+    def rendertex(self):
+        renderfile = os.path.join(self.tmpdir,'report.tex')
+        
+        template = Environment(loader=BaseLoader).from_string(self.template)
+        template_out = template.render(**self.rendervars)
+
+        with open(renderfile, "w",encoding="utf8") as myfile:
+            myfile.write(template_out) 
         
     def get_clique(self):
         self.clique = [ list(k) for k in nx.find_cliques(self.G) if len(k)>2 ]
@@ -34,8 +56,11 @@ class soziogramm(object):
         in_clique = list(set(in_clique))
         not_in_clique = [ k for k in self.G.nodes() if (k not in in_clique) ]
         
-        print("{:} Cliquen:".format(str(len(self.clique))))
-        _ = [print(", ".join(k)) for k in self.clique ]
+        cliques = [ ", ".join(k) for k in self.clique ]
+        cliques_len = str(len(self.clique))
+        
+        self.rendervars['cliquen'] =  "\\item " + "\n\\item ".join(cliques)
+        print("{:} Cliquen:\n".format(cliques_len) + self.rendervars['cliquen'])
         
         if len(not_in_clique)>0: print("\n\nNicht in Cliquen:\n"+", ".join(not_in_clique))
         
@@ -111,4 +136,123 @@ class soziogramm(object):
             cons_in = pd.DataFrame({'name':cons_in.keys(), 'out':cons_in.values()})
             cons = pd.merge(cons_in,cons_out,on='name')
             cons['in-out'] = cons['in']-cons['out']
-            self.cons = cons.sort_values(['in-out'],ascending=[False]).reset_index(drop=True)
+            self.cons = cons.sort_values(['in'],ascending=[False]).reset_index(drop=True)
+            self.rendervars['cons'] = self.cons.to_latex(index=False)
+
+    def run_latex(self,input_filename, output_filename,runinfo=None,latex="/usr/bin/pdflatex",ending='tex'):
+        input_filename = os.path.abspath(input_filename+'.'+ending)
+        input_filename = (os.path.dirname(input_filename),os.path.basename(input_filename))
+
+        output_filename = os.path.abspath(output_filename)
+        output_filename = (os.path.dirname(output_filename),os.path.basename(output_filename))
+        
+        if not isinstance(runinfo,type(None)):
+            print("Compiling {out}.pdf (Run {run}/{total})".format(out=output_filename[1],run=runinfo[0],total=runinfo[1]))
+            
+        cmd = [
+            latex,
+            '-output-format=pdf',
+            '-jobname=' + output_filename[1],
+            ]
+        cmd += [
+            input_filename[1],
+            '>/dev/null 2>&1',
+            ]
+        os.system(" ".join(cmd))
+        
+    def create_pdf(self,input_filename, output_filename,n=2):
+        self.run_latex(input_filename, output_filename)
+        for i in range(n):
+            self.run_latex(input_filename, output_filename,runinfo=(i+1,n))
+
+    def ensure_dir(self,DIR):
+        dirlist = os.path.normpath(DIR).split(os.sep)
+        for i in range(len(dirlist)):
+            tmpdir = os.path.abspath(os.sep.join(dirlist[:i+1]))
+            if not os.path.exists(tmpdir):
+                os.mkdir(tmpdir)
+                
+    def gen_report(self):
+        self.tmpdir = "/tmp/report"
+        self.ensure_dir(self.tmpdir)
+        self.make_soziogramm()
+        self.get_clique()
+        self.save(name='soziogramm1')
+        self.make_soziogramm(directed=True)
+        self.save(name='soziogramm2')
+        self.rendertex()
+        rootdir = os.getcwd()
+        os.chdir(self.tmpdir)
+        pdfname = 'report'
+        self.create_pdf('report',pdfname)
+        shutil.copy(os.path.join(self.tmpdir,pdfname+'.pdf'),os.path.join(rootdir,pdfname+'.pdf'))
+        os.chdir(rootdir)
+
+            
+    def set_template(self):
+        self.template=r"""
+{% raw %}\documentclass[a4paper]{article}
+\usepackage[utf8]{inputenc}
+\usepackage[ddmmyyyy]{datetime}
+\renewcommand{\dateseparator}{.}
+\usepackage{graphicx}
+\usepackage{float}
+\usepackage{booktabs}
+\usepackage[landscape]{geometry}
+\usepackage{fancyhdr}
+\pagestyle{fancy}
+\fancypagestyle{pdfreport}{%
+\fancyheadoffset{0.25cm} 
+\fancyhf{}
+\fancyhead[LE,RO]{\today}
+\fancyhead[RE,LO]{Soziogramm Report}
+\fancyhead[CO,CE]{}
+\fancyfoot[CE,CO]{\leftmark}
+\fancyfoot[CE,CO]{Seite \thepage}
+\fancyfoot[R]{}
+\fancyfoot[L]{\textcopyright\,Hilberg\,\the\year{}}
+}
+\pagestyle{pdfreport}
+\geometry{
+a4paper,
+left=10mm,
+right=10mm,
+top=20mm,
+bottom=20mm,
+}
+% Helvetica font
+\usepackage{helvet}
+\renewcommand{\familydefault}{\sfdefault}
+%
+\setlength\parindent{0pt}
+
+\begin{document}
+
+\begin{minipage}[t]{0.5\textwidth}
+\vspace{0pt}
+Cliquen:
+\begin{itemize}
+{% endraw %}{{ cliquen }}{% raw %}
+\end{itemize}
+\end{minipage}
+\begin{minipage}[t]{0.5\textwidth}
+\vspace{0pt}
+{% endraw %}{{ cons }}{% raw %}
+\end{minipage}
+
+\begin{minipage}[t]{\textwidth}
+\vspace{0.5cm}
+Bild 1: Gegenseitige Verbindungen \par
+\includegraphics[width=\linewidth]{soziogramm1.pdf}\\
+\end{minipage}
+\par
+\begin{minipage}[t]{\textwidth}
+\vspace{0.5cm}
+Bild 2: Alle Verbindungen \par
+\includegraphics[width=\linewidth]{soziogramm2.pdf}\\
+\end{minipage}
+\par
+
+\end{document}
+{% endraw %}
+"""
