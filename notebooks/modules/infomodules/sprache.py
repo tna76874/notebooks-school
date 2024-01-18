38b4098a854b757983be319e7574ad7370c3896f
lmh <dev@hilberg.eu>
2024-01-18 15:10:05 +0100

    module: sprache: add normalizer for german language

--- notebooks/modules/infomodules/sprache.py
+++ notebooks/modules/infomodules/sprache.py
@@ -16,9 +16,20 @@ class sprache:
         print(translation.origin, ' -> ', translation.text)
 
     def erkennen(self, text):
-        result = self.translator.detect(text)
+        normalized_text = self.normalisiere_umlaut(text)
+        result = self.translator.detect(normalized_text)
         print(f"Sprache: {result.lang}\nSo sicher wurde die Sprache erkannt: {result.confidence * 100:.0f}%")
         return result.lang, result.confidence
+
+    def normalisiere_umlaut(self, text):
+        umlaute_dict = {
+            'ae': 'ä', 'Ae': 'Ä', 'AE': 'Ä',
+            'oe': 'ö', 'Oe': 'Ö', 'OE': 'Ö',
+            'ue': 'ü', 'Ue': 'Ü', 'UE': 'Ü'
+        }
+        for key, value in umlaute_dict.items():
+            text = text.replace(key, value)
+        return text
 
 if __name__ == "__main__":
     sprachuebersetzer = sprache()
